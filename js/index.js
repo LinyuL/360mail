b3620d91fa80cf7c77c5e6db4ec325f48ce7dc59
LinyuL <1574178573@qq.com>
2016-05-01 19:38:23 +0800

    360mail
    360商城

---
+++ js/index.js
@@ -0,0 +1,16 @@
+/*$(function(){
+	var inner=$(".inner");
+	var imgs=$(".inner a");
+	console.log(imgs);
+	var imgw=imgs.width();
+	var first=inner:first-child;
+	var index=0;
+	var t=setInterval(move,2000);
+	function move(){
+		inner.animate({marginLeft:-imgw},function(){
+			inner:"first-child".appendTo(inner)
+		})
+	}
+
+
+})*/
