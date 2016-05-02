1cf1802fde111e69da87d9cac0d979caedd54a23
LinyuL <1574178573@qq.com>
2016-05-02 22:15:26 +0800

    360mail

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-/*$(function(){
-	var inner=$(".inner");
+$(function(){
+	/*var inner=$(".inner");
 	var imgs=$(".inner a");
 	console.log(imgs);
 	var imgw=imgs.width();
@@ -10,7 +10,20 @@
 		inner.animate({marginLeft:-imgw},function(){
 			inner:"first-child".appendTo(inner)
 		})
-	}
+	}*/
+	/*返回按钮*/
+	var back=$(".back");
+	console.log(back);
+	back.click(function(){
+		var docu=document.body.scrollTop?document.body:document.documentElement;
+		document.title=docu;
+		docu.animate({scrollTop:0});
+	})
 
+	var as=$(".foot-menu a");
+	as.click(function(){
+		var index=$(this).index();
+		as.removeClass("hot").eq(index).addClass("hot");
+	})
 
-})*/
+})
